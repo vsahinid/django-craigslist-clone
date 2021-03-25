f4e0f2183bf952a0d4769e7d2d2bdc6fbeb6bc51
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-25 10:03:25 -0500

    Added frontend

--- craigslist_clone/urls.py
+++ craigslist_clone/urls.py
@@ -1,7 +1,6 @@
-"""craigslist_clone URL Configuration
-
+"""codedaddies_list URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +13,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from my_app import views
 
 urlpatterns = [
+    path('', include('my_app.urls')),
     path('admin/', admin.site.urls),
 ]
